716c75f2e45cb10d3f86e33a68e69df4035b3318
guohaozhong <guohaozhong@163.com>
2020-11-26 15:12:09 +0800

    update code

--- android/src/main/java/com/hozan/applets/RNAppletsModule.java
+++ android/src/main/java/com/hozan/applets/RNAppletsModule.java
@@ -1,22 +1,71 @@
 
 package com.hozan.applets;
 
+import android.util.Log;
+
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.bridge.WritableMap;
+import com.tencent.mm.opensdk.modelbiz.WXLaunchMiniProgram;
+import com.tencent.mm.opensdk.openapi.IWXAPI;
+import com.tencent.mm.opensdk.openapi.WXAPIFactory;
+
+import org.json.JSONObject;
+
 
 public class RNAppletsModule extends ReactContextBaseJavaModule {
 
-  private final ReactApplicationContext reactContext;
+    private final ReactApplicationContext reactContext;
+    private String Tag = "RNAppletsModule";
 
-  public RNAppletsModule(ReactApplicationContext reactContext) {
-    super(reactContext);
-    this.reactContext = reactContext;
-  }
+    public RNAppletsModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+        this.reactContext = reactContext;
+    }
 
-  @Override
-  public String getName() {
-    return "RNApplets";
-  }
+    @ReactMethod
+    public void callNative(ReadableMap params, Promise promise) {
+        Log.i(Tag, "收到RN指令：" + params);
+        String action = params.getString("action");
+        String data = params.getString("data");
+
+        try {
+            JSONObject obj = new JSONObject(data);
+            Log.i(Tag, obj+"");
+            switch (action) {
+                case "jumpApplets":
+                    String appId = obj.getString("appId"); // 填应用AppId
+                    String userName = obj.getString("userName");
+                    String path = obj.getString("path");
+                    int miniprogramType = obj.getInt("miniprogramType");
+                    IWXAPI api = WXAPIFactory.createWXAPI(reactContext, appId);
+
+                    WXLaunchMiniProgram.Req req = new WXLaunchMiniProgram.Req();
+                    req.userName = userName; // 填小程序原始id
+                    req.path = path;   ///pages/minipropay/minipropay? 拉起小程序页面的可带参路径，不填默认拉起小程序首页，对于小游戏，可以只传入 query 部分，来实现传参效果，如：传入 "?foo=bar"。
+                    req.miniprogramType = miniprogramType;// 可选打开 开发版，体验版和正式版 WXLaunchMiniProgram.Req.MINIPROGRAM_TYPE_TEST
+                    api.sendReq(req);
+//                    WritableMap writableMap= Arguments.createMap();
+//                    writableMap.putString("result","success");
+//                    promise.resolve(writableMap);
+                    break;
+                default:
+                    break;
+            }
+        } catch (Exception e) {
+            promise.reject("失败提示", e);
+        }
+
+
+    }
+
+    @Override
+    public String getName() {
+        return "RNApplets";
+    }
 }
--- android/src/main/java/com/hozan/applets/RNAppletsPackage.java
+++ android/src/main/java/com/hozan/applets/RNAppletsPackage.java
@@ -1,6 +1,7 @@
 
 package com.hozan.applets;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -9,20 +10,20 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
-import com.facebook.react.bridge.JavaScriptModule;
+/**
+ *hozan
+ *2020-11-18
+ */
 public class RNAppletsPackage implements ReactPackage {
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
-      return Arrays.<NativeModule>asList(new RNAppletsModule(reactContext));
-    }
-
-    // Deprecated from RN 0.47
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-      return Collections.emptyList();
+        List<NativeModule> modules = new ArrayList<>();
+        modules.add(new RNAppletsModule(reactContext));
+        return modules;
     }
 
     @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
-      return Collections.emptyList();
+        return Collections.emptyList();
     }
 }
