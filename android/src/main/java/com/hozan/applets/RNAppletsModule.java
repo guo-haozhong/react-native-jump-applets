9912c8cd48684af699852c088d5d56d413935a0c
guohaozhong <guohaozhong@163.com>
2020-12-25 17:14:48 +0800

    android Native module update

--- android/src/main/java/com/hozan/applets/RNAppletsModule.java
+++ android/src/main/java/com/hozan/applets/RNAppletsModule.java
@@ -1,71 +1,152 @@
 
 package com.hozan.applets;
 
+import android.content.Intent;
 import android.util.Log;
 
 import com.facebook.react.bridge.Arguments;
-import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
+import com.tencent.mm.opensdk.constants.ConstantsAPI;
+import com.tencent.mm.opensdk.modelbase.BaseReq;
+import com.tencent.mm.opensdk.modelbase.BaseResp;
 import com.tencent.mm.opensdk.modelbiz.WXLaunchMiniProgram;
 import com.tencent.mm.opensdk.openapi.IWXAPI;
+import com.tencent.mm.opensdk.openapi.IWXAPIEventHandler;
 import com.tencent.mm.opensdk.openapi.WXAPIFactory;
 
-import org.json.JSONObject;
+import java.util.ArrayList;
 
 
-public class RNAppletsModule extends ReactContextBaseJavaModule {
+public class RNAppletsModule extends ReactContextBaseJavaModule implements IWXAPIEventHandler {
 
     private final ReactApplicationContext reactContext;
-    private String Tag = "RNAppletsModule";
+    private String TAG = "RNAppletsModule";
+    private String appId;
+
+    private IWXAPI api = null;
+    private final static String NOT_REGISTERED = "registerApp required.";
+    private final static String INVOKE_FAILED = "WeChat API invoke returns false.";
+    private final static String INVALID_ARGUMENT = "invalid argument.";
 
     public RNAppletsModule(ReactApplicationContext reactContext) {
         super(reactContext);
         this.reactContext = reactContext;
     }
 
-    @ReactMethod
-    public void callNative(ReadableMap params, Promise promise) {
-        Log.i(Tag, "收到RN指令：" + params);
-        String action = params.getString("action");
-        String data = params.getString("data");
-
-        try {
-            JSONObject obj = new JSONObject(data);
-            Log.i(Tag, obj+"");
-            switch (action) {
-                case "jumpApplets":
-                    String appId = obj.getString("appId"); // 填应用AppId
-                    String userName = obj.getString("userName");
-                    String path = obj.getString("path");
-                    int miniprogramType = obj.getInt("miniprogramType");
-                    IWXAPI api = WXAPIFactory.createWXAPI(reactContext, appId);
-
-                    WXLaunchMiniProgram.Req req = new WXLaunchMiniProgram.Req();
-                    req.userName = userName; // 填小程序原始id
-                    req.path = path;   ///pages/minipropay/minipropay? 拉起小程序页面的可带参路径，不填默认拉起小程序首页，对于小游戏，可以只传入 query 部分，来实现传参效果，如：传入 "?foo=bar"。
-                    req.miniprogramType = miniprogramType;// 可选打开 开发版，体验版和正式版 WXLaunchMiniProgram.Req.MINIPROGRAM_TYPE_TEST
-                    api.sendReq(req);
-//                    WritableMap writableMap= Arguments.createMap();
-//                    writableMap.putString("result","success");
-//                    promise.resolve(writableMap);
-                    break;
-                default:
-                    break;
-            }
-        } catch (Exception e) {
-            promise.reject("失败提示", e);
-        }
-
-
-    }
-
     @Override
     public String getName() {
         return "RNApplets";
     }
+
+    private static ArrayList<RNAppletsModule> modules = new ArrayList<>();
+
+    @Override
+    public void initialize() {
+        super.initialize();
+        modules.add(this);
+    }
+    @Override
+    public void onCatalystInstanceDestroy() {
+        super.onCatalystInstanceDestroy();
+        if (api != null) {
+            api = null;
+        }
+        modules.remove(this);
+    }
+
+    public static void handleIntent(Intent intent) {
+        for (RNAppletsModule mod : modules) {
+            mod.api.handleIntent(intent, mod);
+        }
+    }
+
+
+    /**
+     * 注册appid-微信开发平台上申请应用
+     * @param appid
+     * @param callback
+     */
+    @ReactMethod
+    public void registerApp(String appid, Callback callback) {
+        this.appId = appid;
+        api = WXAPIFactory.createWXAPI(this.reactContext, appid, true);
+        callback.invoke( api.registerApp(appid));
+    }
+
+    @ReactMethod
+    public void isWXAppInstalled(Callback callback) {
+        if (api == null) {
+            callback.invoke(NOT_REGISTERED);
+            return;
+        }
+        callback.invoke( api.isWXAppInstalled());
+    }
+
+    /**
+     * 打开微信
+     * @param callback
+     */
+    @ReactMethod
+    public void openWXApp(Callback callback) {
+        if (api == null) {
+            callback.invoke(NOT_REGISTERED);
+            return;
+        }
+        callback.invoke( api.openWXApp());
+    }
+
+    /**
+     * 调起微信小程序
+     * @param data
+     * @param callback
+     */
+    @ReactMethod
+    public void launchMini(ReadableMap data, Callback callback) {
+        if (api == null) {
+            callback.invoke(NOT_REGISTERED);
+            return;
+        }
+        WXLaunchMiniProgram.Req req = new WXLaunchMiniProgram.Req();
+        req.userName = data.getString("userName"); // 填小程序原始id
+        req.path = data.getString("path");                  //拉起小程序页面的可带参路径，不填默认拉起小程序首页
+        req.miniprogramType = data.getInt("miniProgramType");// 可选打开 开发版，体验版和正式版
+        boolean success = api.sendReq(req);
+        if (!success) callback.invoke(INVALID_ARGUMENT);
+    }
+
+    @Override
+    public void onReq(BaseReq baseReq) {
+
+    }
+
+    /**
+     * 微信相关回调
+     * @param baseResp
+     */
+    @Override
+    public void onResp(BaseResp baseResp) {
+        WritableMap map = Arguments.createMap();
+        map.putInt("errCode", baseResp.errCode);
+        map.putString("errStr", baseResp.errStr);
+        map.putString("openId", baseResp.openId);
+        map.putString("transaction", baseResp.transaction);
+
+        if (baseResp.getType() == ConstantsAPI.COMMAND_LAUNCH_WX_MINIPROGRAM) {
+            WXLaunchMiniProgram.Resp resp = (WXLaunchMiniProgram.Resp) baseResp;
+            String extraData = resp.extMsg;
+            map.putString("extraData", extraData);
+            Log.d(TAG, "onResp: "+extraData);
+        }
+
+        this.getReactApplicationContext()
+                .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+                .emit("WeChat_Resp", map);
+    }
+
 }
--- index.js
+++ index.js
@@ -1,6 +1,41 @@
 
-import { NativeModules } from 'react-native';
+import { DeviceEventEmitter,NativeModules } from 'react-native';
+import { EventEmitter } from 'events';
 
 const { RNApplets } = NativeModules;
+const emitter = new EventEmitter();
+
+DeviceEventEmitter.addListener('WeChat_Resp', resp => {
+  emitter.emit('wechat', resp);
+});
+
+/**
+ * 注册appid
+ * @method registerApp
+ * @param {String} appid
+ * @param {Callback} callback
+ */
+export const registerApp = RNApplets.registerApp
+
+/**
+ * 打开小程序
+ * @method launchMini
+ * @param {userName:"小程序原始id",path:"拉起小程序页面的可带参路径",miniProgramType:0}--miniProgramType 拉起小程序的类型. 0-正式版 1-开发版 2-体验版 
+ * @param {Callback} callback
+ */
+export const launchMini=RNApplets.launchMini
+/**
+ * 是否注册appid
+ * @method isWXAppInstalled
+ * @param {Callback} callback
+ */
+export const isWXAppInstalled=RNApplets.isWXAppInstalled
+
+/**
+ * 打开微信
+ * @method openWXApp
+ * @param {Callback} callback
+ */
+export const openWXApp = RNApplets.openWXApp
 
 export default RNApplets;
